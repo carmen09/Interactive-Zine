82677d8a86d2703c2e0c6dd0faced228627e8973
carmen09 <44234186+carmen09@users.noreply.github.com>
2021-03-12 03:24:32 -0500

    0

---
+++ interaction.js
@@ -0,0 +1,89 @@
+let flyingEnglish = document.querySelector(".English");
+let hoverEnglish = document.querySelector(".show-English");
+
+hoverEnglish.addEventListener("mouseover", function(){
+    flyingEnglish.classList.add("active");
+
+})
+
+hoverEnglish.addEventListener("mouseout", function(){
+    flyingEnglish.classList.remove("active");
+
+})
+
+
+function hideEnglish(){
+    flyingEnglish.classList.remove("active");
+}
+
+
+let flyingbridge = document.querySelector(".bridge");
+let hoverMarcoPoloBridge = document.querySelector(".show-bridge");
+
+hoverMarcoPoloBridge.addEventListener("mouseover", function(){
+    flyingbridge.classList.add("active");
+
+})
+
+hoverMarcoPoloBridge.addEventListener("mouseout", function(){
+    flyingbridge.classList.remove("active");
+
+})
+
+
+function hideEmperor(){
+    flyingEmperor.classList.remove("active");
+}
+
+
+let flyingEmperor = document.querySelector(".Emperor");
+let hoverKangxiEmperor = document.querySelector(".show-emperor");
+
+hoverKangxiEmperor.addEventListener("mouseover", function(){
+    flyingEmperor.classList.add("active");
+
+})
+
+hoverKangxiEmperor.addEventListener("mouseout", function(){
+    flyingEmperor.classList.remove("active");
+
+})
+
+
+function hideEmperor(){
+    flyingEmperor.classList.remove("active");
+}
+
+
+let flyingRecord = document.querySelector(".Record");
+let hoverrecordofhistravels = document.querySelector(".show-record");
+
+hoverrecordofhistravels.addEventListener("mouseover", function(){
+    flyingRecord.classList.add("active");
+
+})
+
+hoverrecordofhistravels.addEventListener("mouseout", function(){
+    flyingRecord.classList.remove("active");
+
+})
+
+function hideEmperor(){
+    flyingEmperor.classList.remove("active");
+}
+
+
+let flyingPolo = document.querySelector(".Polo");
+let hoverMarcoPolo = document.querySelector(".show-Polo");
+
+hoverMarcoPolo.addEventListener("mouseover", function(){
+    flyingPolo.classList.add("active");
+
+})
+
+hoverMarcoPolo.addEventListener("mouseout", function(){
+    flyingPolo.classList.remove("active");
+
+})
+
+
